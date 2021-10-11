30db9880b1b86a699a53eec63672f074335ca749
druttis <joakim.dahlstrom@king.com>
2021-10-11 12:14:03 +0200

    test sys

--- src/main/java/org/dru/dusaf/ecs/test/TestSystem.java
+++ src/main/java/org/dru/dusaf/ecs/test/TestSystem.java
@@ -5,29 +5,37 @@ import org.dru.dusaf.ecs.*;
 import java.awt.*;
 
 public class TestSystem implements EcsSystem {
+    public static final int NUM_ENTITIES = 1000000;
+    public static final int NUM_ITERATIONS = 100;
+
     private EcsMapping<Point> pointMapping;
+    private EcsMapping<Dimension> dimensionMapping;
 
     @Override
     public void map(final EcsMappings mappings) {
         pointMapping = mappings.getMapping(Point.class);
+        dimensionMapping = mappings.getMapping(Dimension.class);
     }
 
     @Override
     public void bind(final EcsBinder binder) {
-        binder.bindAnyOf(pointMapping);
+        binder.bindAllOf(pointMapping, dimensionMapping);
     }
 
     @Override
     public void initialize(final EcsEngine engine) {
-        final EcsEntity entity = engine.spawnEntity();
-        pointMapping.setComponent(entity, new Point(0, 100));
+        for (int i = 0; i < NUM_ENTITIES; i++) {
+            final EcsEntity entity = engine.spawnEntity();
+            pointMapping.setComponent(entity, new Point(0, 0));
+            dimensionMapping.setComponent(entity, new Dimension(0, 0));
+        }
     }
 
     @Override
     public void update(final EcsEngine engine, final Iterable<EcsEntity> entities) {
         entities.forEach(entity -> {
             pointMapping.getComponent(entity).x++;
-            System.out.println(pointMapping.getComponent(entity));
+            dimensionMapping.getComponent(entity).width++;
         });
     }
 
@@ -37,10 +45,21 @@ public class TestSystem implements EcsSystem {
     }
 
     public static void main(String[] args) {
+        System.out.printf("number of entities    : %d\n", NUM_ENTITIES);
         final EcsEngine engine = new EcsEngine();
         engine.addSystem(new TestSystem());
-        for (int i = 0; i < 100; i++) {
+        System.out.printf("number of iterations  : %d\n", NUM_ITERATIONS);
+        final long startNanos = System.nanoTime();
+        for (int i = 0; i < NUM_ITERATIONS; i++) {
             engine.update();
         }
+        final long endNanos = System.nanoTime();
+        final double totalNanos = endNanos - startNanos;
+        final double nanosPerIteration = totalNanos / NUM_ITERATIONS;
+        final double nanosPerEntity = nanosPerIteration / NUM_ENTITIES;
+        System.out.println("------------------------------------");
+        System.out.printf("total time            : %.0f ms.\n", totalNanos / 1000000d);
+        System.out.printf("time per iteration    : %.2f ms.\n", nanosPerIteration / 1000000d);
+        System.out.printf("time per entity       : %.5f ns.\n", nanosPerEntity);
     }
 }
